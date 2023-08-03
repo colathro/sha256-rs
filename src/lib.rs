61ad5dc1d084cefbb1de705d5b8132a4943ad335
colton@spacedoglabs.com <colton@spacedoglabs.com>
2023-08-03 15:17:23 -0700

    cleaned up warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::ops::Range;
-
 const SHA256_CONSTANTS: &[u32] = &[
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -179,7 +177,6 @@ impl SHA256 {
 mod tests {
     use super::*;
     use hex::FromHex;
-    use sha256::digest;
 
     #[test]
     fn empty() {
